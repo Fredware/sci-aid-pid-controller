574cdad89c7b4a50f66e0da49a73e3a74d4a2014
Fredi R. Mino <35937021+Fredware@users.noreply.github.com>
2023-11-07 21:11:34 -0700

    Working program: Two-thread blinkin LEDs. TODO: replace blinking with I2C sampling; replace blinking with Analog Write.

---
+++ src/main.c
@@ -0,0 +1,78 @@
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+/*Node Identifiers by Device Tree Alias*/
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+
+#define LED0 DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN0 DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS0 DT_GPIO_FLAGS(LED0_NODE, gpios)
+
+#define LED1 DT_GPIO_LABEL(LED1_NODE, gpios)
+#define PIN1 DT_GPIO_PIN(LED1_NODE, gpios)
+#define FLAGS1 DT_GPIO_FLAGS(LED1_NODE, gpios)
+
+#define STACK_SIZE 2000
+struct k_thread coop_thread;
+K_THREAD_STACK_DEFINE(coop_stack, STACK_SIZE);
+
+int counter;
+bool led_is_on;
+
+void thread_entry_func(void){
+   const struct device *dev;
+   dev = device_get_binding(LED1);
+   bool led_is_on = true;
+   int ret = gpio_pin_configure(dev, PIN1, GPIO_OUTPUT_ACTIVE | FLAGS1);
+
+   struct k_timer led_timer;
+   k_timer_init(&led_timer, NULL, NULL);
+
+   while(1){
+    counter = counter+1;
+    led_is_on = !led_is_on;
+    gpio_pin_set(dev, PIN1, (int)led_is_on);
+    k_timer_start(&led_timer, K_MSEC(2000), K_NO_WAIT);
+    k_timer_status_sync(&led_timer);
+   } 
+}
+
+void main(void)
+{
+    const struct device *dev;
+    led_is_on = true;
+    int ret;
+
+    dev = device_get_binding(LED0);
+    
+    k_thread_create
+    (
+        &coop_thread,
+        coop_stack,
+        STACK_SIZE,
+        (k_thread_entry_t) thread_entry_func,
+        NULL,
+        NULL,
+        NULL,
+        K_PRIO_COOP(7),
+        0,
+        K_NO_WAIT
+    );
+    if(dev == NULL){
+        return;
+    }
+    ret = gpio_pin_configure(dev, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
+    if(ret < 0)
+    {
+        return;
+    }
+
+    while(1){
+        led_is_on = !led_is_on;
+        gpio_pin_set(dev, PIN0, (int)led_is_on);
+        k_msleep(500);
+    }
+}
